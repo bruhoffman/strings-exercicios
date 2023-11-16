3c19bbc243736245a60583d7b9cd412e1d1e517b
bruhoffman <bruhoffman@gmail.com>
2023-11-16 15:59:19 -0300

    Todos os exercícios feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,13 @@
-function imprimirComidasFavoritas (){
+function imprimirComidasFavoritas() {
+    const nomeUsuario = prompt("Olá, qual o seu nome?");
+    const comida1 = prompt("Digite a sua 1ª comida favorita.");
+    const comida2 = prompt("Digite a tua 2ª comida favorita.");
+    const comida3 = prompt("E fim, digita a tua 3ª comida favorita.");
 
+    console.log(`Estas são as comidas favoritas de ${nomeUsuario}:
+    \n- ${comida1}
+    \n- ${comida2}
+    \n- ${comida3}`);
 }
+
+imprimirComidasFavoritas();
